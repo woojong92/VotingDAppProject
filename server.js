944ec49a98c4da4bd0f8a80b6d624f0ffd1cf7a9
Woojong Kim <woojong92@gmail.com>
2018-11-27 17:55:01 +0900

    new file

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var Web3 = require('web3');
+
+app.listen(8080, function() {
+    console.log('Server Start');
+})
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
