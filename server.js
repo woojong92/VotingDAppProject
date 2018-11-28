359cd72cf9a21df9850ae31f8d5c54cb9832de16
Woojong Kim <woojong92@gmail.com>
2018-11-28 23:55:39 +0900

    modified

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var Web3 = require('web3');
 
-app.listen(8080, function() {
+app.listen(3000, function() {
     console.log('Server Start');
 })
 
-
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', function(req, res){
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.writeHead(200, { 'Content-Type' : 'text/html'});
+            res.end(data);
+        }
+    });
 });
